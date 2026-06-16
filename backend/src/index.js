c669858fb89dcee6e11ea6771849c08a328601e2
Carmel Karni <53690458+CarmelKarni@users.noreply.github.com>
2026-06-16 12:59:24 +0200

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import cors from 'cors'
+import todoApi from './api.js'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-const PORT = 3001
+app.use('/api/todos', todoApi)
+
+const PORT = process.env.PORT || 3001
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
