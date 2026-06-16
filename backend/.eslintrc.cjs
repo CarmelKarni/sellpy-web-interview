7ef1fb38ae69aeb4aaf1f2776557c84288e3ee0d
Carmel Karni <53690458+CarmelKarni@users.noreply.github.com>
2026-06-16 13:15:39 +0200

    Update .eslintrc.cjs

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -1,11 +1,15 @@
 module.exports = {
   env: {
     node: true,
+    es2020: true,
   },
   extends: 'eslint:recommended',
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
+  },
+  globals: {
+    Map: 'readonly',
   },
   rules: {},
 }
